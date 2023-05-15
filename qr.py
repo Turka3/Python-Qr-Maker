a6cdbdc5a1b0c9ab6b1bb3eb30654e0f59a5109f
Turka3 <muzcuahmet01@gmail.com>
2023-05-15 10:11:29 +0300

    Python Qr

---
+++ qr.py
@@ -0,0 +1,16 @@
+import requests
+from PIL import Image
+from io import BytesIO
+
+url = "https://" + input("Url Giriniz : ")
+
+endpoint = "https://api.qrserver.com/v1/create-qr-code/"
+
+orn = f"?data={url}&amp;size=500x500"
+color = "&color=black"
+
+response = requests.get(endpoint + orn + color)
+
+img = Image.open(BytesIO(response.content))
+
+img.save("QR.png")
